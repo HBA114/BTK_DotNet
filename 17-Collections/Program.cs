a0f6225207d24c896efe3b31feee6dc54e64db8d
HBA114 <55455410+HBA114@users.noreply.github.com>
2022-05-06 20:37:24 +0300

    Dictionary Update on Collections

--- 17-Collections/Program.cs
+++ 17-Collections/Program.cs
@@ -1,55 +1,28 @@
-﻿// ArrayList();
-List<string> list = new List<string>();
-list.Add("listItem1");
+﻿// List0();
+// List1();
 
-List<Customer> customerList = new List<Customer>();
+Dictionary<string,dynamic> map = new Dictionary<string,dynamic>();
+map.Add("name","Hasan");
+map.Add("age", 22);
 
-customerList.Add(new Customer { id = 1, firstName = "Hasan" });
-customerList.Add(new Customer { id = 2, firstName = "Engin" });
 
-var customer2 = new Customer { id = 3, firstName = "Ali" };
-customerList.Add(customer2);
-customerList.AddRange(new Customer[2]
+Console.WriteLine(map["name"]);
+Console.WriteLine(map["age"]);
+// Console.WriteLine(map["friend"]); // keyNotFounr Error
+
+foreach (var item in map)
 {
-    new Customer { id = 4, firstName = "Ahmet" },
-    new Customer { id = 5, firstName = "Mustafa" },
-});
-
-// customerList.Clear(); // removes all elements from list
-
-var isContains = customerList.Contains(new Customer { id = 1, firstName = "Hasan" });
-Console.WriteLine("isContains : {0}",isContains);   // false because we dont look to values we are looking for reference.
-var isContains2 = customerList.Contains(customer2);
-Console.WriteLine("isContains2 : {0}",isContains2);
-
-Console.WriteLine("Customer2 in index : {0}",customerList.IndexOf(customer2));
-Console.WriteLine("Customer2 in index reverse search : {0}",customerList.LastIndexOf(customer2));
-
-customerList.Add(customer2); // reverse search will find this and give index of this
-Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
-Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
-
-customerList.Insert(0,customer2);  // insert item to given index
-Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
-Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
-
-customerList.Remove(customer2); // will remove first and exits
-Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
-Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
-
-customerList.RemoveAll(c => c.firstName == "Ahmet"); // will remove all that have fistname "Ahmet"
-
-
-foreach (var customer in customerList)
-{
-    Console.WriteLine("id : {0} , name : {1}", customer.id, customer.firstName);
+    Console.WriteLine("{0} : {1}",item.Key,item.Value);
 }
-var count = customerList.Count();
-Console.WriteLine("Count : {0}", count);
+
+Console.WriteLine(map.ContainsKey("name"));
+Console.WriteLine(map.ContainsKey("friend"));
+Console.WriteLine(map.ContainsValue("Hasan"));
+Console.WriteLine(map.ContainsValue("Ahmet"));
 
 Console.ReadLine();
 
-static void ArrayList()
+static void List0()
 {
     // string[] cities = new string[2] { "Ankara", "Konya" };
     // this array have 2 values and cant add a 3rd value
@@ -79,6 +52,57 @@ static void ArrayList()
     {
         Console.WriteLine(country);
     }
+}
+
+static void List1()
+{
+    List<string> list = new List<string>();
+    list.Add("listItem1");
+
+    List<Customer> customerList = new List<Customer>();
+
+    customerList.Add(new Customer { id = 1, firstName = "Hasan" });
+    customerList.Add(new Customer { id = 2, firstName = "Engin" });
+
+    var customer2 = new Customer { id = 3, firstName = "Ali" };
+    customerList.Add(customer2);
+    customerList.AddRange(new Customer[2]
+    {
+    new Customer { id = 4, firstName = "Ahmet" },
+    new Customer { id = 5, firstName = "Mustafa" },
+    });
+
+    // customerList.Clear(); // removes all elements from list
+
+    var isContains = customerList.Contains(new Customer { id = 1, firstName = "Hasan" });
+    Console.WriteLine("isContains : {0}", isContains);   // false because we dont look to values we are looking for reference.
+    var isContains2 = customerList.Contains(customer2);
+    Console.WriteLine("isContains2 : {0}", isContains2);
+
+    Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
+    Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
+
+    customerList.Add(customer2); // reverse search will find this and give index of this
+    Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
+    Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
+
+    customerList.Insert(0, customer2);  // insert item to given index
+    Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
+    Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
+
+    customerList.Remove(customer2); // will remove first and exits
+    Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
+    Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
+
+    customerList.RemoveAll(c => c.firstName == "Ahmet"); // will remove all that have fistname "Ahmet"
+
+
+    foreach (var customer in customerList)
+    {
+        Console.WriteLine("id : {0} , name : {1}", customer.id, customer.firstName);
+    }
+    var count = customerList.Count();
+    Console.WriteLine("Count : {0}", count);
 }
 
 class Customer
