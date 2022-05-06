81789b336eebdff1a27acad027cee8ab8ea57c30
HBA114 <55455410+HBA114@users.noreply.github.com>
2022-05-06 20:23:19 +0300

    Update on Collections

--- 17-Collections/Program.cs
+++ 17-Collections/Program.cs
@@ -7,10 +7,45 @@ List<Customer> customerList = new List<Customer>();
 customerList.Add(new Customer { id = 1, firstName = "Hasan" });
 customerList.Add(new Customer { id = 2, firstName = "Engin" });
 
+var customer2 = new Customer { id = 3, firstName = "Ali" };
+customerList.Add(customer2);
+customerList.AddRange(new Customer[2]
+{
+    new Customer { id = 4, firstName = "Ahmet" },
+    new Customer { id = 5, firstName = "Mustafa" },
+});
+
+// customerList.Clear(); // removes all elements from list
+
+var isContains = customerList.Contains(new Customer { id = 1, firstName = "Hasan" });
+Console.WriteLine("isContains : {0}",isContains);   // false because we dont look to values we are looking for reference.
+var isContains2 = customerList.Contains(customer2);
+Console.WriteLine("isContains2 : {0}",isContains2);
+
+Console.WriteLine("Customer2 in index : {0}",customerList.IndexOf(customer2));
+Console.WriteLine("Customer2 in index reverse search : {0}",customerList.LastIndexOf(customer2));
+
+customerList.Add(customer2); // reverse search will find this and give index of this
+Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
+Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
+
+customerList.Insert(0,customer2);  // insert item to given index
+Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
+Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
+
+customerList.Remove(customer2); // will remove first and exits
+Console.WriteLine("Customer2 in index : {0}", customerList.IndexOf(customer2));
+Console.WriteLine("Customer2 in index reverse search : {0}", customerList.LastIndexOf(customer2));
+
+customerList.RemoveAll(c => c.firstName == "Ahmet"); // will remove all that have fistname "Ahmet"
+
+
 foreach (var customer in customerList)
 {
     Console.WriteLine("id : {0} , name : {1}", customer.id, customer.firstName);
 }
+var count = customerList.Count();
+Console.WriteLine("Count : {0}", count);
 
 Console.ReadLine();
 
