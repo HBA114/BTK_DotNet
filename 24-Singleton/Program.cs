af4d105354345cbb5df0dc6f4c6f10602129ad84
HBA114 <hbasriayhaner114@gmail.com>
2023-02-21 12:18:53 +0300

    Added Thread Safe Algorithm with Lock

--- 24-Singleton/Program.cs
+++ 24-Singleton/Program.cs
@@ -6,22 +6,22 @@ customerManager.Save();
 public class CustomerManager
 {
     static CustomerManager? _customerManager;
+    static object _lockObject = new object();
     private CustomerManager()
     {
 
     }
     public static CustomerManager CreateAsSingleton()
     {
-        return _customerManager ?? (_customerManager = new CustomerManager());
-        
-        /*
-        if (_customerManager == null)
+        lock (_lockObject)
         {
-            _customerManager = new CustomerManager();
+            if (_customerManager == null)
+            {
+                _customerManager = new CustomerManager();
+            }
         }
-
+        
         return _customerManager;
-        */
     }
     public void Save()
     {
