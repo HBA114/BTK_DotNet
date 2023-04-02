05821df4954b70f0dd326f97ef22b7ecd4be5a2a
HBA114 <hbasriayhaner114@gmail.com>
2023-04-02 18:15:57 +0300

    Added comment fro PrototypeDesign

--- 27-Prototype/Program.cs
+++ 27-Prototype/Program.cs
@@ -6,7 +6,7 @@
     City = "Konya"
 };
 
-
+//* customer2 is completely different object, not reference of customer1
 Customer customer2 = (Customer)customer1.Clone();   // if not casted default is Person type
 customer2.FirstName = "Veli";
 
