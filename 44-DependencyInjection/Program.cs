586f8912c8627c31e04d90b03c59b6e194c311c4
HBA114 <hbasriayhaner114@gmail.com>
2023-05-17 15:54:09 +0300

    Added dependency injection sample project

---
+++ 44-DependencyInjection/Program.cs
@@ -0,0 +1,44 @@
+﻿#region main
+
+// ProductManager productManager = new ProductManager(new EfProductDal());
+ProductManager productManager = new ProductManager(new NHProductDal());
+productManager.Save();
+
+#endregion
+
+
+public interface IProductDal
+{
+    void Save();
+}
+
+public class EfProductDal : IProductDal
+{
+    public void Save()
+    {
+        Console.WriteLine($"Saved with EF");
+    }
+}
+
+public class NHProductDal : IProductDal
+{
+    public void Save()
+    {
+        Console.WriteLine($"Saved with NH");
+    }
+}
+
+public class ProductManager
+{
+    private IProductDal _productDal;
+
+    public ProductManager(IProductDal productDal)
+    {
+        _productDal = productDal;
+    }
+
+    public void Save()
+    {
+        _productDal.Save();
+    }
+}
