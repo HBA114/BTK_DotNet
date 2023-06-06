acb711e1903904d8e28dde110daf69e06d171042
HBA114 <hbasriayhaner114@gmail.com>
2023-06-06 18:44:38 +0300

    Added new test method samples

--- 47-TestDrivenDevelopment/47.4-Asserts/Asserts.Tests/UnitTest1.cs
+++ 47-TestDrivenDevelopment/47.4-Asserts/Asserts.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 namespace Asserts.Tests;
 
 [TestClass]
@@ -11,7 +12,7 @@ public class UnitTest1
 
         double actual = Math.Sqrt(inputValue);
 
-        Assert.AreEqual(expected: expected, actual: actual, $"Square Root of {inputValue} should be {expected}.");
+        Assert.AreEqual(expected: expected, actual: actual, message: $"Square Root of {inputValue} should be {expected}.");
     }
 
     [TestMethod]
@@ -33,7 +34,7 @@ public class UnitTest1
         string expected = "HELLO";
         string actual = "hello";
 
-        Assert.AreEqual(expected: expected, actual: actual, true);
+        Assert.AreEqual(expected: expected, actual: actual, ignoreCase: true);
     }
 
     [TestMethod]
@@ -43,5 +44,77 @@ public class UnitTest1
         var actual = Math.Pow(5, 0);
 
         Assert.AreNotEqual(notExpected: notExpected, actual: actual);
+    }
+
+    [TestMethod]
+    public void TestMethod5()
+    {
+        var numbers = new byte[] { 1, 2, 3 };
+        var otherNumbers = numbers;
+
+        numbers[0] = 4;
+
+        Assert.AreSame(expected: numbers, actual: otherNumbers);
+    }
+
+    [TestMethod]
+    public void TestMethod6()
+    {
+        int a = 10;
+        int b = a;
+
+        //! Assert.AreSame failed. Do not pass value types to AreSame().
+        // Assert.AreSame(expected: a, actual: b, message: "AreSame Failed");
+        Assert.AreEqual(expected: a, actual: b, message: "AreEqual Failed");
+        Assert.AreNotSame(notExpected: a, actual: b, message: "AreNotSame Failed");
+    }
+
+    [TestMethod]
+    public void TestMethod7()
+    {
+        Assert.AreEqual(expected: 1, actual: 1);
+        Assert.Inconclusive("This test is not sufficient");
+    }
+
+    [TestMethod]
+    public void TestMethod8()
+    {
+        var number = 5m;
+
+        Assert.IsInstanceOfType(value: number, expectedType: typeof(decimal));
+        Assert.IsNotInstanceOfType(value: number, wrongType: typeof(int));
+    }
+
+    [TestMethod]
+    public void TestMethod9()
+    {
+        Assert.IsTrue(condition: 10 % 2 == 0);
+        Assert.IsFalse(condition: 10 % 2 == 1);
+    }
+
+    [TestMethod]
+    public void TestMethod10()
+    {
+        List<string> names = new() { "Hasan", "Mustafa", "Ahmet" };
+
+        var firstNameStartsWithLetterH = names.FirstOrDefault(x => x.StartsWith("H"));
+        var firstNameStartsWithLetterB = names.FirstOrDefault(x => x.StartsWith("B"));
+
+        Assert.IsNull(value: firstNameStartsWithLetterB, message: "IsNull Failed!");
+        Assert.IsNotNull(value: firstNameStartsWithLetterH, message: "IsNotNull Failed!");
+    }
+
+    [TestMethod]
+    public void TestMethod11()
+    {
+        try
+        {
+            var number = 5;
+            int result = number / 0;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Assert.Fail(message: $"Test Failed, Error message was: {ex.Message}");
+        }
     }
 }
