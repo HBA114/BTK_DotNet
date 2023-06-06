0657437167f911b00ff0507476b6ed37bdc10a22
HBA114 <hbasriayhaner114@gmail.com>
2023-06-06 19:07:18 +0300

    Added new collection assert sample test methods

--- 47-TestDrivenDevelopment/47.5-CollectionAsserts/CollectionAsserts.Tests/UnitTest1.cs
+++ 47-TestDrivenDevelopment/47.5-CollectionAsserts/CollectionAsserts.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 namespace CollectionAsserts.Tests;
 
 [TestClass]
@@ -25,5 +26,62 @@ public class UnitTest1
         newUsers.Add("Ahmet");
 
         CollectionAssert.AreEqual(expected: _users, actual: newUsers);
+    }
+
+    [TestMethod]
+    public void ElementsShouldBeSameSortCanBeDifferent()
+    {
+        List<string> newUsers = new List<string>();
+
+        newUsers.Add("Mustafa");
+        newUsers.Add("Ahmet");
+        newUsers.Add("Hasan");
+
+        CollectionAssert.AreEquivalent(expected: _users, actual: newUsers);
+    }
+
+    [TestMethod]
+    public void ElementsOrSortShouldBeDifferent()
+    {
+        List<string> newUsers = new List<string>();
+
+        newUsers.Add("Mustafa");
+        newUsers.Add("Hasan");
+        newUsers.Add("Ahmet");
+
+        CollectionAssert.AreNotEqual(notExpected: _users, actual: newUsers);
+    }
+
+    [TestMethod]
+    public void ElementsShouldBeDifferent()
+    {
+        List<string> newUsers = new List<string>();
+
+        newUsers.Add("Hasan");
+        newUsers.Add("Mustafa");
+        newUsers.Add("Ahmet");
+        newUsers.Add("Basri");
+
+        CollectionAssert.AreNotEquivalent(expected: _users, actual: newUsers);
+    }
+
+    [TestMethod]
+    public void ElementsShouldNotBeNull()
+    {
+        CollectionAssert.AllItemsAreNotNull(collection: _users);
+    }
+
+    [TestMethod]
+    public void ElementsShouldBeUnique()
+    {
+        CollectionAssert.AllItemsAreUnique(collection: _users);
+    }
+
+    [TestMethod]
+    public void AllElementsShouldBeSameType()
+    {
+        ArrayList list = new() { "Hasan", "Mustafa", "Ahmet" };
+
+        CollectionAssert.AllItemsAreInstancesOfType(collection: list, expectedType: typeof(string));
     }
 }
